c753bf7a13d1d6386d320324210ed5d06723cdd4
Uniliva Alves <uniliva@hotmail.com>
2019-04-08 20:17:14 -0300

    Estrutura basica do projeto

---
+++ src/main/java/br/com/uniliva/ebao/EbaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.uniliva.ebao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbaoApplication.class, args);
+	}
+
+}
